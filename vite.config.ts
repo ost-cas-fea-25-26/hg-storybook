475de95b2a58f21741bf6466a8f6ab8770a4fbcc
Patrick <patrickbrunner96@hotmail.ch>
2025-10-03 22:03:37 +0200

    wip: trying to get package to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,31 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
-import path from 'path';
+import path, { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
+    manifest: true,
     lib: {
       entry: resolve(__dirname, './lib/index.ts'),
-      name: 'react-beautiful-timeline',
+      name: 'hg-storybook',
       fileName: (format) => `index.${format}.ts`,
+      cssFileName: 'hg-storybook',
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'tailwindcss'],
+      external: ['react', 'tailwindcss'],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
           tailwindcss: 'tailwindcss',
+          react: 'React',
         },
       },
     },
     sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts({ rollupTypes: true }), tailwindcss()],
+  plugins: [react(), dts(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'lib'),
