2390562439626b470a3a0acc5eeaa76ab7d89ab3
jannikjost <jannik.jost@clavisit.com>
2025-10-22 19:17:52 +0200

    feat: update storybook story for button

--- lib/button/Button.tsx
+++ lib/button/Button.tsx
@@ -1,6 +1,7 @@
 import { Button as HeadlessButton } from '@headlessui/react';
 import clsx from 'clsx';
-import React, { ReactNode, useMemo } from 'react';
+import { ReactNode, useMemo } from 'react';
+import React from 'react';
 
 interface Props {
   variant: 'primary' | 'secondary' | 'gradient';
@@ -20,17 +21,17 @@ export default function Button({ children, size, variant, onClick, circle }: Pro
     } else if (variant === 'secondary') {
       return 'bg-secondary hover:bg-secondary-700 active:bg-secondary-800 focus:outline-secondary-200';
     } else if (variant === 'gradient') {
-      return 'bg-linear-to-r from-pink-500 to-primary hover:from-[-12%] hover:to-62% active:from-[-15%] active:to-40% focus:outline-primary-200';
+      return 'bg-gradient-to-r from-pink-500 to-primary hover:from-[-12%] hover:to-[62%] active:from-[-15%] active:to-[40%] focus:outline-primary-200';
     }
   }, [variant]);
 
   const sizeClassname = useMemo(() => {
     if (size === 'small') {
       return 'p-2';
-    } else if (size === 'medium') {
-      return 'p-3';
     } else if (size === 'large') {
       return 'py-4 px-5';
+    } else {
+      return 'p-3';
     }
   }, [size]);
 
