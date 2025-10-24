b81423414d8f66c303d591e82deaeed814a719aa
jannikjost <jannik.jost@clavisit.com>
2025-10-24 13:42:59 +0200

    feat: create deployment on github pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import { withoutVitePlugins } from '@storybook/builder-vite';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
@@ -12,6 +13,11 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  async viteFinal(config) {
+    config.plugins = await withoutVitePlugins(config.plugins, ['vite:dts']);
+
+    return config;
   },
 };
 export default config;
