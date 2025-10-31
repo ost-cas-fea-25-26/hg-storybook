121d42c934c2ed6761e827a1aec3fd1a07403b2f
Patrick <patrickbrunner96@hotmail.ch>
2025-10-31 09:10:25 +0100

    feat: adds timed button component

---
+++ lib/timedButton/TimedButton.stories.tsx
@@ -0,0 +1,109 @@
+import { Cross, Repost, Tick } from '@/icon';
+import TimedButton from '@/timedButton/TimedButton.tsx';
+import { Meta, StoryObj } from '@storybook/react-vite';
+import React from 'react';
+
+const meta = {
+  component: TimedButton,
+} satisfies Meta<typeof TimedButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Simple: Story = {
+  args: {
+    animationDuration: 1500,
+    onClick: (_e) => {
+      console.log('hello world');
+    },
+    childrenOnClick: (
+      <>
+        <Tick size={'xs'} />
+        {'Button Copied!'}
+      </>
+    ),
+    children: (
+      <>
+        <Repost size={'xs'} />
+        {'Button Label'}
+      </>
+    ),
+  },
+};
+
+export const Async: Story = {
+  args: {
+    animationDuration: 1500,
+    onClick: async (_e) => {
+      return await new Promise((resolve) => {
+        setTimeout(() => {
+          console.log('hello world async');
+          resolve();
+        }, 1500);
+      });
+    },
+    childrenOnClick: (
+      <>
+        <Tick size={'xs'} />
+        {'Button Copied!'}
+      </>
+    ),
+    children: (
+      <>
+        <Repost size={'xs'} />
+        {'Button Label'}
+      </>
+    ),
+  },
+};
+
+export const AsyncWithError: Story = {
+  args: {
+    animationDuration: 1500,
+    onClick: async (_e) => {
+      return await new Promise((_, reject) => {
+        setTimeout(() => {
+          reject();
+        }, 1500);
+      });
+    },
+    childrenOnError: (
+      <span className={'text-red-400 flex items-center gap-2'}>
+        <Cross color={'text-red-400'} size={'xs'} />
+        <span>Button Error</span>
+      </span>
+    ),
+    childrenOnClick: (
+      <>
+        <Tick size={'xs'} />
+        {'Button Copied!'}
+      </>
+    ),
+    children: (
+      <>
+        <Repost size={'xs'} />
+        {'Button Label'}
+      </>
+    ),
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    animationDuration: 1500,
+    disabled: true,
+    onClick: () => {},
+    childrenOnClick: (
+      <>
+        <Tick size={'xs'} />
+        {'Button Copied!'}
+      </>
+    ),
+    children: (
+      <>
+        <Repost size={'xs'} />
+        {'Button Label'}
+      </>
+    ),
+  },
+};
---
+++ lib/timedButton/TimedButton.tsx
@@ -0,0 +1,78 @@
+import { Button as HeadlessButton } from '@headlessui/react';
+import clsx from 'clsx';
+import React, { ReactNode, useState } from 'react';
+
+type Props = {
+  animationDuration: number;
+  children: ReactNode;
+  onClick: (e?: React.SyntheticEvent<HTMLButtonElement>) => void | Promise<void>;
+  childrenOnClick: ReactNode;
+  childrenOnError?: ReactNode;
+  disabled?: boolean;
+};
+
+/**
+ * React Component to Provide timed action on click
+ * @param animationDuration the amount of time (ms) the childrenOnClick are displayed.
+ * in Async mode: the amount of time the error (in case of rejected Promise) is displayed
+ * @param children button content when idle
+ * @param childrenOnClick button content when clicked as long as 'animation' in progress
+ * @param childrenOnError button content in async mode if the onClick promise is rejected
+ * @param onClick action of what happens on click
+ * @param disabled if true: nothing happens on click
+ * */
+export default function TimedButton({
+  animationDuration = 1500,
+  children,
+  childrenOnClick,
+  childrenOnError,
+  onClick = () => {},
+  disabled = false,
+}: Props) {
+  const [animating, setAnimating] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
+  const disabledClassName = 'cursor-not-allowed opacity-50';
+
+  return (
+    <HeadlessButton
+      className={clsx(
+        `
+        focus:outline-secondary-200
+         focus:outline-2
+         cursor-pointer
+         duration-200 
+         font-semibold 
+         flex 
+         justify-center 
+         items-center 
+         gap-2 
+         text-secondary
+         `,
+        {
+          [disabledClassName]: disabled,
+        }
+      )}
+      onClick={() => {
+        if (disabled) return;
+        setAnimating(true);
+        const maybePromise = onClick();
+        if (typeof maybePromise?.then === 'function') {
+          return maybePromise
+            .then(() => setAnimating(false))
+            .catch(() => {
+              setAnimating(false);
+              setError(true);
+              setTimeout(() => setError(false), animationDuration);
+            });
+        } else {
+          setTimeout(() => {
+            setAnimating(false);
+          }, animationDuration);
+        }
+      }}
+    >
+      {error ? childrenOnError : animating ? childrenOnClick : children}
+    </HeadlessButton>
+  );
+}
