1e1cb857af43e0ca083edeb26520ae271c2b12c8
Patrick <patrickbrunner96@hotmail.ch>
2025-10-31 12:12:48 +0100

    feat: adds Modal component

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ import './styles/index.css';
 
 export { default as Button } from './button/Button';
 export { default as Toggle } from './toggle/Toggle';
+export { default as Modal } from './modal/Modal';
