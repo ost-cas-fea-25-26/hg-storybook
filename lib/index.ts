9f27b39b7fe8434f5c471ac51684f4aaeede04db
veracahrim <patrickbrunner96@hotmail.ch>
2025-11-02 14:27:33 +0100

    feat: adds timed button component (#42)
    * feat: adds timed button component
    
    * fix: implements review feedback
    - extract inline function to const
    - fix formatting of tailwind

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,6 @@ import './styles/index.css';
 
 export { default as Button } from './button/Button';
 export { default as Toggle } from './toggle/Toggle';
+export { default as TimedButton } from './timedButton/TimedButton';
 export { default as Modal } from './modal/Modal';
 export { default as Input } from './input/Input';
