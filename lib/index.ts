0d2badc23df4714964c83d35d4034ea1d550304b
jannikjost <jannik.jost@clavisit.com>
2025-11-02 14:58:02 +0100

    feat: add label component

--- lib/index.ts
+++ lib/index.ts
@@ -4,3 +4,5 @@ export { default as Button } from './button/Button';
 export { default as Toggle } from './toggle/Toggle';
 export { default as Input } from './input/Input';
 export { default as Textarea } from './textarea/Textarea';
+export { default as Label } from './label/Label';
+export { Field } from '@headlessui/react';
---
+++ lib/label/Label.tsx
@@ -0,0 +1,14 @@
+import { Label as HeadlessLabel } from '@headlessui/react';
+import React, { ComponentProps, ReactNode } from 'react';
+
+export interface Props extends ComponentProps<'label'> {
+  children: ReactNode;
+}
+
+export default function Label({ children, ...props }: Props) {
+  return (
+    <HeadlessLabel className="mb-1 font-semibold text-slate-700" {...props}>
+      {children}
+    </HeadlessLabel>
+  );
+}
