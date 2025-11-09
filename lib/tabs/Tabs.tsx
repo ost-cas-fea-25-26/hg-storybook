21f3327adc7a7babde091f11ffbadd8f74d48e2c
jannikjost <jannik.jost@clavisit.com>
2025-11-09 20:58:01 +0100

    fix: run prettier

--- lib/tabs/Tabs.tsx
+++ lib/tabs/Tabs.tsx
@@ -1,27 +1,42 @@
-import { Tab as HeadlessTab, TabGroup as HeadlessTabGroup, TabList as HeadlessTabList, TabPanel as HeadlessTabPanel } from '@headlessui/react';
-import React from 'react';
+import {
+  Tab as HeadlessTab,
+  TabGroup as HeadlessTabGroup,
+  TabList as HeadlessTabList,
+  TabPanel as HeadlessTabPanel,
+} from '@headlessui/react'
+import React from 'react'
 
 export interface TabProps {
-  defaultIndex?: number;
-  children: React.ReactNode;
-  onChange?: (index: number) => void;
+  defaultIndex?: number
+  children: React.ReactNode
+  onChange?: (index: number) => void
 }
 
 export function TabGroup({ defaultIndex = 0, children, onChange }: TabProps) {
-  return <HeadlessTabGroup defaultIndex={defaultIndex} onChange={(index) => {
-    if (onChange) onChange(index);
-  }}>{children}</HeadlessTabGroup>;
+  return (
+    <HeadlessTabGroup
+      defaultIndex={defaultIndex}
+      onChange={(index) => {
+        if (onChange) onChange(index)
+      }}
+    >
+      {children}
+    </HeadlessTabGroup>
+  )
 }
 
 export function TabList({ children }: { children: React.ReactNode }) {
-  return <HeadlessTabList className="flex flex-row p-1 group transition-all duration-300 ease-in-out rounded-sm w-fit bg-secondary-200">{children}</HeadlessTabList>;
+  return (
+    <HeadlessTabList className="flex flex-row p-1 group transition-all duration-300 ease-in-out rounded-sm w-fit bg-secondary-200">
+      {children}
+    </HeadlessTabList>
+  )
 }
 
-export function Tab({
-  children
-}: { children: React.ReactNode }) {
+export function Tab({ children }: { children: React.ReactNode }) {
   return (
-    <HeadlessTab className="
+    <HeadlessTab
+      className="
       font-semibold rounded-sm leading-none text-lg 
       px-3 py-2.5
       cursor-pointer
@@ -38,12 +53,13 @@ export function Tab({
       last:group-hover:data-selected:ml-1
       last:ml-2
       transition-all duration-300 ease-in-out
-    ">
+    "
+    >
       {children}
     </HeadlessTab>
-  );
+  )
 }
 
 export function TabPanel({ children, className }: { children: React.ReactNode; className?: string }) {
-  return <HeadlessTabPanel className={className}>{children}</HeadlessTabPanel>;
+  return <HeadlessTabPanel className={className}>{children}</HeadlessTabPanel>
 }
